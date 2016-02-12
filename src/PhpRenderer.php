22fb2f28a8ad8ebafe75b71b7621fd6182097f16
JunSangPil <jun85664396@gmail.com>
2016-02-12 17:19:27 +0900

    use layout

--- src/PhpRenderer.php
+++ src/PhpRenderer.php
@@ -17,58 +17,59 @@ use Psr\Http\Message\ResponseInterface;
  */
 class PhpRenderer
 {
-    /**
-     * @var string
-     */
-    protected $templatePath;
+  /**
+   * @var string
+   */
+  protected $templatePath;
 
-    /**
-     * SlimRenderer constructor.
-     *
-     * @param string $templatePath
-     */
-    public function __construct($templatePath = "")
-    {
-        $this->templatePath = $templatePath;
+  /**
+   * SlimRenderer constructor.
+   *
+   * @param string $templatePath
+   */
+  public function __construct($templatePath = "")
+  {
+    $this->templatePath = $templatePath;
+  }
+
+  /**
+   * Render a template
+   *
+   * $data cannot contain template as a key
+   *
+   * throws RuntimeException if $templatePath . $template does not exist
+   *
+   * @param \ResponseInterface $response
+   * @param                    $template
+   * @param array              $data
+   *
+   * @return ResponseInterface
+   *
+   * @throws \InvalidArgumentException
+   * @throws \RuntimeException
+   */
+  public function render(ResponseInterface $response, $template, array $data = [], $layout = "/layout.php")
+  {
+    if (isset($data['template'])) {
+      throw new \InvalidArgumentException("Duplicate template key found");
     }
 
-    /**
-     * Render a template
-     *
-     * $data cannot contain template as a key
-     *
-     * throws RuntimeException if $templatePath . $template does not exist
-     *
-     * @param \ResponseInterface $response
-     * @param                    $template
-     * @param array              $data
-     *
-     * @return ResponseInterface
-     *
-     * @throws \InvalidArgumentException
-     * @throws \RuntimeException
-     */
-    public function render(ResponseInterface $response, $template, array $data = [])
-    {
-        if (isset($data['template'])) {
-            throw new \InvalidArgumentException("Duplicate template key found");
-        }
-
-        if (!is_file($this->templatePath . $template)) {
-            throw new \RuntimeException("View cannot render `$template` because the template does not exist");
-        }
-
-        $render = function ($template, $data) {
-            extract($data);
-            include $template;
-        };
-
-        ob_start();
-        $render($this->templatePath . $template, $data);
-        $output = ob_get_clean(); 
-
-        $response->getBody()->write($output);
-        
-        return $response;
+    if (!is_file($this->templatePath . $template)) {
+      throw new \RuntimeException("View cannot render `$template` because the template does not exist");
     }
+
+    $render = function ($template, $data) {
+      extract($data);
+      include $template;
+    };
+
+    ob_start();
+    $render($this->templatePath . $template, $data);
+    $output = ob_get_clean(); 
+
+    include $this->templatePath . $layout;
+
+    return $response;
+  }
 }
+
